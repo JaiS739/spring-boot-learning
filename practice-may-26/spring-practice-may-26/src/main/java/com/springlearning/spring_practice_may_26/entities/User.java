16636b358af9b4e6072e9ac6601336d0db0151df
jai-turinton <jaikrishna.singh@turinton.com>
2026-05-13 23:53:42 +0530

    learn: jwt in java create and retrieve id

---
+++ practice-may-26/spring-practice-may-26/src/main/java/com/springlearning/spring_practice_may_26/entities/User.java
@@ -0,0 +1,26 @@
+package com.springlearning.spring_practice_may_26.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+}
--- practice-may-26/spring-practice-may-26/src/test/java/com/springlearning/spring_practice_may_26/SpringPracticeMay26ApplicationTests.java
+++ practice-may-26/spring-practice-may-26/src/test/java/com/springlearning/spring_practice_may_26/SpringPracticeMay26ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.springlearning.spring_practice_may_26;
 
+import com.springlearning.spring_practice_may_26.entities.User;
+import com.springlearning.spring_practice_may_26.services.JWTService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringPracticeMay26ApplicationTests {
 
+	@Autowired
+	private JWTService jwtService;
+
 	@Test
 	void contextLoads() {
+		User user = new User(5L, "John Doe", "Jd!2321", "ADMIN");
+		String token = jwtService.generateToken(user);
+		System.out.println("token - " + token);
+
+		Long id = jwtService.getUserIdFromToken(token);
+		System.out.println(id);
 	}
 
 }
